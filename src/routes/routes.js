a15fc30c186a1141dbd2fccea2922f5d0ad16bba
Vinicius Fonseca <vinicius.fonseca20093004@gmail.com>
2024-06-03 02:26:33 -0300

    Ajustes

---
+++ src/routes/routes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  
+} from "react-router-dom";
+import Home from "../containers/Home";
+
+export default function App() {
+  return (
+    <Router>
+        <Routes>
+          <Route path="/" Component={Home}/>
+    
+        </Routes>
+    </Router>
+  );
+}
